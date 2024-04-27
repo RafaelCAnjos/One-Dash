89d2b0fc1f147f94225a5a7fa860f2c66b153e33
rafaelcanjos <rafael_cost123@hotmail.com>
2024-04-27 13:34:18 -0300

    meu primeiro commit

---
+++ js/cadastro-cliente.js
@@ -0,0 +1,12 @@
+let nome = document.getElementById('nome').value;
+let sobrenome = document.getElementById('sobrenome').value;
+let cidade = document.getElementById('cidade').value;
+let email = document.getElementById('email').value;
+let cpf = document.getElementById('cpf').value;
+let cnpj = document.getElementById('cnpj').value;
+let telefone = document.getElementById('telefone').value;
+let botaoCadastro = document.getElementById('botaoCadastro');
+
+botaoCadastro.addEventListener("click", function() {
+
+})
